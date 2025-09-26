b8d8c99b233ad083d5e7c0fec96a40f490ce8714
Sumit <sumitnagpure1@gmail.com>
2025-09-26 13:07:56 +0530

    class notes

---
+++ Class/25-09/one.py
@@ -0,0 +1,40 @@
+# %% [markdown]
+# ### Data-types
+
+# %% [markdown]
+# 
+
+# %%
+i=10
+print("type: ",type(i))
+print("id: ",id(i))
+print("value: ",i)
+
+# %%
+j=10
+print(id(j))
+
+# %%
+print(i==j) #both var have values
+
+# %%
+print(i is j) #refer to the same object location
+
+# %% [markdown]
+# ### Float
+
+# %%
+k=2.23
+print("type: ",type(k))
+
+# %%
+c= complex(i,j)
+
+# %%
+'''ArithmeticError
+System;'''
+
+# %%
+
+
+
